2e74cfdf213cc36acaa8b52ec4262a53c9afebce
Connor Savage <75349013+connorsavage@users.noreply.github.com>
2023-04-26 15:29:31 -0700

    implemented AI api for automated replies

--- capitalizeserver.js
+++ capitalizeserver.js
@@ -1,5 +1,8 @@
 import net from "net";
 
+const API_KEY = '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';
+const API_URL = 'https://api.openai.com/v1/completions';
+
 const server = net.createServer((socket) => {
   console.log(
     "Connection from",
@@ -8,7 +11,7 @@ const server = net.createServer((socket) => {
     socket.remotePort
   );
 
-  socket.on("data", (buffer) => {
+  socket.on("data", async (buffer) => {
     console.log(
       "Request from",
       socket.remoteAddress,
@@ -16,8 +19,26 @@ const server = net.createServer((socket) => {
       socket.remotePort
     );
     //output
-    socket.write(`${buffer.toString()}\n`);
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-002',
+        prompt: buffer.toString(),
+        temperature: 0.7,
+        max_tokens: 2048,
+      }),
+    });
+    const responseText = await response.text();
+    const responseJSON = JSON.parse(responseText);
+    const chatbotResponse = responseJSON.choices[0].text;
+    socket.write(`${chatbotResponse}\n`);
   });
+
+
   socket.on("end", () => {
     console.log("Closed", socket.remoteAddress, "port", socket.remotePort);
   });
