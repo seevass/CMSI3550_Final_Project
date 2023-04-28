dd5f963fc7d79c4aac3c3d74f41fe09175a51bd2
Sebastian Cruz <scruz17@lion.lmu.edu>
2023-04-28 01:08:39 -0700

    fixed formatting

--- dateserver.js
+++
@@ -1,8 +0,0 @@
-import net from "net";
-
-// A use-once date server. Clients get the date on connection and that's it!
-const server = net.createServer((socket) => {
-  socket.end(`${new Date()}\n`);
-});
-
-server.listen(59090);
